3b66bb17165099caa0d2270ab473cc4ec40329fd
Bode Chris Adewole <chris@biznesxpo.com>
2026-09-24 17:49:26 +0200

    remove stale generated JavaScript duplicate

--- apps/frontend/lib/auth-client.js
+++
@@ -1,9 +0,0 @@
-"use client";
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authClient = void 0;
-var react_1 = require("better-auth/react");
-var plugins_1 = require("better-auth/client/plugins");
-exports.authClient = (0, react_1.createAuthClient)({
-    plugins: [(0, plugins_1.emailOTPClient)()],
-});
