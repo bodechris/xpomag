c9b7a32bcd9c800cb822b3d461410cd23470b469
Bode Chris Adewole <chris@biznesxpo.com>
2026-09-24 17:20:05 +0200

    remove duplicate generated frontend route files

--- apps/frontend/app/(auth)/layout.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = AuthLayout;
-require("./auth.css");
-require("./flow.css");
-function AuthLayout(_a) {
-    var children = _a.children;
-    return <>{children}</>;
-}
